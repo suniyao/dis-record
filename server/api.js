23b5f3427d933e413c2c57f4c38cf6d9bd51bc5a
StephY0728 <3011316681@qq.com>
2025-03-08 21:29:17 +0800

    update api.js

--- server/api.js
+++ server/api.js
@@ -1,3 +1,14 @@
 const mongoose = require("mongoose");
 
 const Activity = require("./models/activity");
+
+router.get("/activity", (req, res) => {
+    Activity.find({}).then((activities) => res.send(activities));
+})
+
+router.post("/activity", (req, res) => {
+    const newActivity = new Activity ({
+
+    });
+    newActivity.save().then((activity) => res.send(activity));
+})
